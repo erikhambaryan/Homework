cc814004e911fbcada87b2d565091c48d1e5f4b2
erikhambaryan <36689029+erikhambaryan@users.noreply.github.com>
2023-10-28 23:21:19 +0400

    Add files via upload

---
+++ kalibri.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+
+int main(int argc, char* argv[])
+{
+        for(int o=argc-1; o>0; o--)
+        {
+            std::cout<<argv[o]<<" ";
+        }
+            std::cout<<std::endl;
+            return 0;
+}
