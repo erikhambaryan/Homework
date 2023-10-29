6d3e85be9f2f3f2e63c68cc270b22dbf1e5d72cd
erikhambaryan <36689029+erikhambaryan@users.noreply.github.com>
2023-10-29 23:21:24 +0400

    Add files via upload

---
+++ SHADY.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <unistd.h>
+
+int main () {
+
+
+    fork();
+	fork();
+	fork();
+	std::cout << "\n";
+	return 0;
+}
+
+//this could be malitios code that would grant me reverse shell of your PC
+//but instead u get eminem
+
+//Now this looks like a job for me
+//So everybody just follow me
+//'Cause we need a little controversy
+//'Cause it feels so empty without me
+//
+//I said this looks like a job for me
+//So everybody, just follow me
+//'Cause we need a little controversy
+//'Cause it feels so empty without me
