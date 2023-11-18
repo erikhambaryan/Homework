757fabb5fe5c9d5c585edf1d3a3b55e5427705b6
erikhambaryan <36689029+erikhambaryan@users.noreply.github.com>
2023-11-19 00:59:51 +0400

    Add files via upload

---
+++ 19.11.23(2nd)/makefile.cpp
@@ -0,0 +1,10 @@
+CXX=g++
+CXXFLAGS=-Wall
+
+all: process_interaction
+
+process_interaction: process_interaction.cpp
+	$(CXX) $(CXXFLAGS) -o process_interaction 19.11.23(2nd).cpp
+
+clean:
+	rm -f process_interaction
